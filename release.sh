793a96a7da679a61af6f4fb832318f82f37d37eb
starfish-pengolin <starfishcotton@gmail.com>
2026-01-04 12:19:08 -0500

    Prepare v1.0.0 release

--- release.sh
+++ release.sh
@@ -54,3 +54,60 @@ gh release create $VERSION $ZIP_NAME \
 echo "Release created! Direct download link:"
 echo "https://github.com/starfish-pengolin/$APP_NAME/releases/download/$VERSION/$ZIP_NAME"
 
+#!/bin/bash
+# Auto-version GitHub release script for DesignTranslatorApp
+# Includes hidden credit automatically
+
+APP_NAME="DesignTranslatorApp"
+REPO="git@github.com:starfish-pengolin/DesignTranslatorApp.git"
+RELEASE_TITLE="DesignTranslatorApp Release"
+RELEASE_NOTES="Stable release of DesignTranslatorApp"
+
+# 1) Determine new version
+LATEST_TAG=$(git tag --sort=-v:refname | head -n1)
+if [[ -z "$LATEST_TAG" ]]; then
+  VERSION="v1.0.0"
+else
+  IFS='.' read -r MAJOR MINOR PATCH <<< "${LATEST_TAG//v/}"
+  PATCH=$((PATCH+1))
+  VERSION="v$MAJOR.$MINOR.$PATCH"
+fi
+
+ZIP_NAME="${APP_NAME}_${VERSION}.zip"
+
+# 2) Add hidden credit
+echo "Powered by Mosh3L" > hidden_credit.txt
+
+# 3) Zip project cleanly
+zip -r $ZIP_NAME . \
+  -x "*.git*" \
+  -x "venv/*" \
+  -x "*.pyc" \
+  -x "__pycache__/*" \
+  -x ".ssh/*" \
+  -x "tmp/*" \
+  -x "frontend_dist/*" \
+  -x "node_modules/*" \
+  -x "*.log" \
+  -x "*.zip"
+
+# 4) Commit changes
+git add .
+git commit -m "Prepare $VERSION release" || echo "Nothing to commit"
+
+# 5) Push to GitHub
+git branch -M main
+git remote set-url origin $REPO
+git push -u origin main
+
+# 6) Create GitHub release
+gh release create $VERSION $ZIP_NAME \
+  --title "$RELEASE_TITLE $VERSION" \
+  --notes "$RELEASE_NOTES"
+
+# 7) Print permanent download link
+echo "Release created! Direct download link:"
+echo "https://github.com/starfish-pengolin/$APP_NAME/releases/download/$VERSION/$ZIP_NAME"
+
+# 8) Clean up
+rm hidden_credit.txt
